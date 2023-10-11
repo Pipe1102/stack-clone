5a6b23a4e98ee8e2ca0b3c03fe8e1f36571962d0
filip-jovanovic <pipejovanovic558@gmail.com>
2023-10-11 14:16:44 +0200

    bugfix

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -53,10 +53,7 @@ export async function deleteUser(params: DeleteUserParams) {
     if (!user) {
       throw new Error("User not found");
     }
-    // eslint-disable-next-line no-unused-vars
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
 
     await Question.deleteMany({ author: user._id });
 
